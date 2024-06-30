28bedbc08933988dcfc9b8eac1687be9f72314db
dpfls0922 <dpfls0922@naver.com>
2024-07-01 02:52:37 +0900

    add: 계산기 문제 url 추가

--- week7/myinsta/urls.py
+++ week7/myinsta/urls.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 from django.urls import path, include
 from posts.views import url_view, url_parameter_view, function_view, class_view
 from posts.views import index #방금 만든 인덱스 임포트해서 넣어주고 #posts>urls.py의 url을 posts/뒤에 붙이도록 만들음
+from posts.views import calculator
 
 urlpatterns = [
+    path('calculator/', calculator),
     path('admin/', admin.site.urls),
     path('url/', url_view),
     path('url/<str:username>/', url_parameter_view),
