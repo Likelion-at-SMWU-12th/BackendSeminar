f1f6aa243949f5190ccb48d4436d9a04dfb07515
dpfls0922 <dpfls0922@naver.com>
2024-07-01 09:59:41 +0900

    fix: 이미지 url 클릭 시 오류 해결

--- week7/myinsta/urls.py
+++ week7/myinsta/urls.py
@@ -3,6 +3,8 @@ from django.urls import path, include
 from posts.views import url_view, url_parameter_view, function_view, class_view
 from posts.views import index #방금 만든 인덱스 임포트해서 넣어주고 #posts>urls.py의 url을 posts/뒤에 붙이도록 만들음
 from posts.views import calculator
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('calculator/', calculator),
@@ -16,3 +18,5 @@ urlpatterns = [
     path('posts/', include('posts.urls', namespace='posts')), 
     path('accounts/', include('accounts.urls', namespace='accounts')), 
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
