de3f3ad1e1faa97b965cda5d500bb93fa00be552
wonandonly <wann416@sookmyung.ac.kr>
2024-09-23 21:49:22 +0900

    [feat] tweet기능 추가

--- Spring/week05/client/src/main/java/com/example/client/controller/RestTemplateController.java
+++ Spring/week05/client/src/main/java/com/example/client/controller/RestTemplateController.java
@@ -1,13 +1,25 @@
 package com.example.client.controller;
 
 import com.example.client.dto.MemberDto;
+import com.example.client.dto.Tweet;
 import com.example.client.service.RestTemplateService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.http.HttpMethod;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/rest-template")
 public class RestTemplateController {
+    private static final Logger log = LoggerFactory.getLogger(WebClientController.class);
+    private String getSlowServiceUri() {
+        return "http://localhost:9090/api/v1/slow"; 
+    }
 
     private final RestTemplateService restTemplateService;
 
@@ -38,5 +50,21 @@ public class RestTemplateController {
     @PostMapping("/header")
     public ResponseEntity<MemberDto> postWithHeader() {
         return restTemplateService.postWithHeader();
+    }
+
+    @GetMapping("/tweets-blocking")
+    public List<Tweet> getTweetsBlocking() {
+        log.info("Starting BLOCKING Controller!");
+        final String uri = getSlowServiceUri();
+
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<List<Tweet>> response = restTemplate.exchange(
+                uri, HttpMethod.GET, null,
+                new ParameterizedTypeReference<List<Tweet>>(){});
+
+        List<Tweet> result = response.getBody();
+        result.forEach(tweet -> log.info(tweet.toString()));
+        log.info("Exiting BLOCKING Controller!");
+        return result;
     }
 }
--- Spring/week05/client/src/main/java/com/example/client/controller/WebClientController.java
+++ Spring/week05/client/src/main/java/com/example/client/controller/WebClientController.java
@@ -1,14 +1,25 @@
 package com.example.client.controller;
 
 import com.example.client.dto.MemberDto;
+import com.example.client.dto.Tweet;
 import com.example.client.service.WebClientService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
 public class WebClientController {
+    private static final Logger log = LoggerFactory.getLogger(WebClientController.class);
+
+    private String getSlowServiceUri() {
+        return "http://localhost:9090/api/v1/slow";
+    }
 
     @Autowired
     private WebClientService webClientService;
@@ -21,6 +32,20 @@ public class WebClientController {
     @GetMapping("/post-member")
     public Mono<MemberDto> postMember() {
         return webClientService.postWithParamAndBody();
+    }
+
+    @GetMapping(value = "/tweets-non-blocking", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<Tweet> getTweetsNonBlocking() {
+        log.info("Starting NON-BLOCKING Controller!");
+        Flux<Tweet> tweetFlux = WebClient.create()
+                .get()
+                .uri(getSlowServiceUri())
+                .retrieve()
+                .bodyToFlux(Tweet.class);
+
+        tweetFlux.subscribe(tweet -> log.info(tweet.toString()));
+        log.info("Exiting NON-BLOCKING Controller!");
+        return tweetFlux;
     }
 }
 
